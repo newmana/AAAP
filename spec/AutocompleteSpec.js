4c72b3b2417318fa029d15b7fb7d7ebda90dc59a
Andrew Newman <andrew.newman@sdx.com.au>
2011-03-28 16:35:03 +1000

    Refactoring test

--- spec/AutocompleteSpec.js
+++ spec/AutocompleteSpec.js
@@ -10,29 +10,30 @@ describe("autocomplete", function() {
     auto.initialize();
   });
 
-  it("valid suggestions causes update to DOM", function() {
-    auto.currentValue = 'a';
-    auto.suggestions = ['banana', 'peach'];
-    auto.data = [ 'fruit_1', 'fruit_2'];
-    auto.suggest();
-    expect(auto.enabled).toBe(true);
-    expect($('container')).toContain('div[title=banana]');
-    expect($('container')).toContain('div[title=peach]');
-  }); 
+  describe("check suggest", function() {
+    beforeEach(function() {
+      auto.currentValue = 'a';
+      auto.suggestions = ['banana', 'peach'];
+      auto.data = [ 'fruit_1', 'fruit_2'];
+      auto.suggest();
+    });
   
-  it("move down can go no further than maximum item", function() {
-    auto.currentValue = 'a';
-    auto.suggestions = ['banana', 'peach'];
-    auto.data = [ 'fruit_1', 'fruit_2'];
-    auto.suggest();
-    checkMoveDown(0, "banana");
-    checkMoveDown(1, "peach");
-    checkMoveDown(1, "peach");
+    it("valid suggestions causes update to DOM", function() {
+      expect(auto.enabled).toBe(true);
+      expect($('container')).toContain('div[title=banana]');
+      expect($('container')).toContain('div[title=peach]');
+    }); 
+  
+    it("move down can go no further than maximum item", function() {
+      checkMoveDown(0, "banana");
+      checkMoveDown(1, "peach");
+      checkMoveDown(1, "peach");
+    });
+
+    function checkMoveDown(expectedIndex, expectedValue) {
+      auto.moveDown();
+      expect(auto.selectedIndex).toEqual(expectedIndex);
+      expect(auto.el.value).toEqual(expectedValue);
+    }
   });
-  
-  function checkMoveDown(expectedIndex, expectedValue) {
-    auto.moveDown();
-    expect(auto.selectedIndex).toEqual(expectedIndex);
-    expect(auto.el.value).toEqual(expectedValue);
-  }
 });
