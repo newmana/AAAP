083f9b7993ecc05a77e14a6a3b8d9edb31ba7c13
Andrew Newman <andrew.newman@sdx.com.au>
2011-03-28 16:09:35 +1000

    First test

--- spec/AutocompleteSpec.js
+++ spec/AutocompleteSpec.js
@@ -1,11 +1,24 @@
 describe("autocomplete", function() {
 
   beforeEach(function() {
-    setFixtures('<input type="text" name="wunderbar" id="query"/>');         
-    auto = new Autocomplete("query", {});
+    setFixtures('<input type="text" name="wunderbar" id="query"/><div id="container"/>');
+    auto = new Autocomplete("query", {
+      serviceUrl : "/object",
+      container : "container"
+    });
+    auto.isDomLoaded = true;
+    auto.initialize();
   });
 
-  it("can run", function() {
-    expect(document.getElementById('query')).not.toBeNull();
+  it("valid suggestions causes update to DOM", function() {
+    expect($('container')).toContain('#anonymous_element_1');;
+    expect($('container')).toContain('#anonymous_element_2');;
+    auto.currentValue = 'a';
+    auto.suggestions = ['banana', 'peach'];
+    auto.data = [ 'fruit_1', 'fruit_2'];
+    auto.suggest();
+    expect(auto.enabled).toBe(true);
+    expect($('container')).toContain('div[title=banana]');
+    expect($('container')).toContain('div[title=peach]');
   }); 
 });
